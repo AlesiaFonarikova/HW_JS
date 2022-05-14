f5237400e39537e5b5082edeaf384c2584d247e4
AlesiaFonarikova <100477127+AlesiaFonarikova@users.noreply.github.com>
2022-05-14 21:21:54 +0300

    the 2nd HW update

--- validation.js
+++ validation.js
@@ -8,7 +8,8 @@
 //  8. Должна быть хотя бы одна @
 //  9. Строка не должна быть пустой
 
-let str = "Hello";
+let str = "Hello6@";
+let counter = 0;
 function validation(str) {
   if (str.trim() == "")
     console.log('String "', str, '" is empty. Please, enter your string.');
@@ -19,36 +20,57 @@ function validation(str) {
         str,
         '" is too short. The string must contain at least 5 symbols.'
       );
+    else {
+      counter++;
+    }
     if (str.length > 64)
       console.log(
         'String "',
         str,
         '" is too long. The string must contain no more than 64 symbols.'
       );
+    else {
+      counter++;
+    }
     if (!/[0-9]+/.test(str))
       console.log(
         'String "',
         str,
         '" doesnt contain numbers. The string must contain at least 1 number.'
       );
+    else {
+      counter++;
+    }
     if (!/[a-zA-Zа-яА-Я]+/.test(str))
       console.log(
         'String "',
         str,
         '" doesnt contain letters. The string must contain letters.'
       );
+    else {
+      counter++;
+    }
     if (!/[A-ZА-Я]+/.test(str))
       console.log(
         'String "',
         str,
         '" doesnt contain uppercase letters. The string must contain at least 1 uppercase letter.'
       );
+    else {
+      counter++;
+    }
     if (!str.includes("@"))
       console.log(
         'String "',
         str,
         '" doesnt contain @. The string must contain at least 1 @-symbol.'
       );
+    else {
+      counter++;
+    }
+  }
+  if (counter === 6) {
+    console.log("Validation passed!!!");
   }
 }
 validation(str);
